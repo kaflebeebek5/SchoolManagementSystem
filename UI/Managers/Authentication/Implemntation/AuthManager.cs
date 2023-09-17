068819124d5ebf8a6921d7cb8ce46cf20f3a2996
Bibek Kafle <kaflebeebek5@gmail.com>
2023-09-17 12:06:09 +0545

    UI And API Gateway Initial Commit

--- UI/Managers/Authentication/Implemntation/AuthManager.cs
+++ UI/Managers/Authentication/Implemntation/AuthManager.cs
@@ -4,16 +4,24 @@ using UI.ViewModels;
 
 namespace UI.Managers.Authentication.Implemntation
 {
-    public class AuthManager:IAuthManager
+    public class AuthManager : IAuthManager
     {
+        HttpClient _httpClient = new HttpClient
+        {
+            Timeout = TimeSpan.FromMinutes(5) // Set a longer timeout, e.g., 5 minutes
+        };
         private readonly IConfiguration configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public AuthManager(IConfiguration configuration)
+        public AuthManager(IConfiguration configuration, IHttpClientFactory httpClientFactory)
         {
             this.configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+            _httpClient = _httpClientFactory.CreateClient("ApiGateway");
         }
         public async Task<IResponse> Login(AuthenticationViewModel viewModel)
         {
+            var Data = await _httpClient.GetAsync("Auth/Login");
             return null;
         }
     }
